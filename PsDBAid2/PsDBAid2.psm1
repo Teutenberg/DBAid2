26fa829a307333e6b74dd892ceb7b0b8dd211af4
Wayne Teutenberg <Wayne_Teutenberg@bnz.co.nz>
2019-09-11 16:02:41 +1200

    added to own repo

--- PsDBAid2/PsDBAid2.psm1
+++
@@ -1,6 +0,0 @@
-<#
-    PsDBAid2.psm1 is a helper module for scripted activities. This module will contain functions to help simplify scripting and increase productivity. 
-#>
-Import-Module SqlServer
-Import-Module SqlServerDsc
-#Import-Module ReportingServicesTools
