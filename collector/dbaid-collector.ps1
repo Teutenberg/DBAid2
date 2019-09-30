1715738d77c195ac9c39c989f813aef1b7e91e52
Wayne Teutenberg <Wayne_Teutenberg@bnz.co.nz>
2019-09-30 16:43:20 +1300

    issue#8 added sql destination

--- collector/dbaid-collector.ps1
+++ collector/dbaid-collector.ps1
@@ -1,44 +1,97 @@
 ﻿Param(
-    [parameter(Mandatory=$false)]
-    [string[]]$SqlServer = 'SETTSPSQLD01',
+    [parameter(Mandatory)]
+    [string[]]$CollectSqlServer = 'SETTSQLD05\INST2017,4319',
 
-    [parameter(Mandatory=$false)]
-    [bool]$UpdateExecTimestamp = 0
+    [parameter()]
+    [string]$CollectDatabase = '_dbaid',
+
+    [parameter()]
+    [Switch]$UpdateExecTimestamp,
+
+    [parameter()]
+    [System.String]$OutputXmlFilePath,
+
+    [parameter()]
+    [Switch]$ZipXml,
+
+    [parameter()]
+    [string[]]$DatamartSqlServer = 'SETTSQLD05\INST2017,4319',
+
+    [parameter()]
+    [string]$DatamartDatabase = '_dbaid'
 )
-CD $PSScriptRoot
-$OutputPath = $PSScriptRoot
 
-foreach ($Instance in $SqlServer) {
-    $InstanceTag = (Invoke-Sqlcmd -ServerInstance $Instance -Database '_dbaid' -Query 'EXEC [system].[get_instance_tag]')[0]
-    $ProcedureList = (Invoke-Sqlcmd -ServerInstance $Instance -Database '_dbaid' -Query "SELECT [proc]=QUOTENAME(SCHEMA_NAME([schema_id])) + N'.' + QUOTENAME([name]) FROM sys.objects WHERE [type] = 'P' AND SCHEMA_NAME([schema_id]) = 'collector'").proc
+$Timestamp = Get-Date -Format 'yyyyMMddHHmm'
+
+foreach ($CollectServer in $CollectSqlServer) {
+    Write-Verbose -Message "Connecting to: $CollectServer"
+    $InstanceTag = Invoke-Sqlcmd -ServerInstance $CollectServer -Database $CollectDatabase -Query 'EXEC [system].[get_instance_tag]' | Select-Object -ExpandProperty instance_tag
+    $ProcedureList = Invoke-Sqlcmd -ServerInstance $CollectServer -Database $CollectDatabase -Query "EXEC [system].[get_procedure_list] @schema_name = 'collector'"
 
     # EXPOR DATA #
-    foreach ($proc in $ProcedureList) {
-        $ProcedureTag = $proc.Substring($proc.IndexOf('_') + 1).Replace(']','')
-        $OutputFile = Join-Path $OutputPath "$InstanceTag`_$ProcedureTag`_$(Get-Date -Format 'yyyyMMddHHmm').xml"
+    foreach ($Procedure in $ProcedureList) {
+        $ProcSchema = $Procedure.Schema 
+        $ProcName = $Procedure.Name
+        $ProcQuery = "EXEC [$ProcSchema].[$ProcName] "
 
-        $dt = Invoke-Sqlcmd -ServerInstance $Instance -Database '_dbaid' -Query "EXEC $proc @update_execution_timestamp = $UpdateExecTimestamp" -OutputAs DataTables
-    
-        if ($dt) {
-            $dt.TableName = $ProcedureTag
-        } else {
-            $dt = New-Object System.Data.Datatable($ProcedureTag)
+        if ($UpdateExecTimestamp) {
+            $ProcQuery = $ProcQuery + "@update_execution_timestamp=1"
         }
-  
-        $dt.WriteXml($OutputFile, "System.Data.XmlWriteMode"::WriteShema)
+
+        Write-Verbose -Message "Executing: $ProcQuery"
+        $dt = Invoke-Sqlcmd -ServerInstance $CollectServer -Database $CollectDatabase -Query $ProcQuery -OutputAs DataTables
+
+        if ($dt) {
+            $dt.TableName = $ProcName 
+        } else {
+            $dt = New-Object System.Data.Datatable($ProcName)
+        }
+
+        if ($OutputXmlFilePath) {
+            Write-Verbose -Message "Outputting XML to: $OutputXmlFilePath"
+            $FileName = $InstanceTag + '_' + $Procedure.Name + '_' + $Timestamp + '.xml'
+            $OutputFile = Join-Path $OutputXmlFilePath $FileName
+            $dt.WriteXml($OutputFile, "System.Data.XmlWriteMode"::WriteShema)
+        }
+
+        foreach ($DatamartServer in $DatamartSqlServer) {
+            $DestTable = '[datamart].[' + $ProcName + ']'
+            $LoadType = $InstanceTag + '_' + $Procedure.Name
+
+            try { # try bulk copy data into destination table
+                $cn = new-object System.Data.SqlClient.SqlConnection("Data Source=$DatamartServer;Integrated Security=SSPI;Initial Catalog=$DatamartDatabase");
+                $bc = New-Object System.Data.SqlClient.SqlBulkCopy($cn)
+                $bc.BatchSize = 10000
+                $bc.BulkCopyTimeout = 1000
+                $bc.DestinationTableName = $DestTable
+                $cn.Open()
+                $bc.WriteToServer($dt)
+		        $logInsert = "INSERT INTO [_dbaid].[datamart].[load_log] ([load_type]) VALUES (N'$LoadType')"
+		        (New-Object System.Data.SqlClient.SqlCommand($logInsert, $cn)).ExecuteNonQuery() | Out-Null
+        
+            } catch {
+                $_.Exception | Write-Output
+            } finally {
+                $bc.Close()
+                $cn.Close()
+            }
+        }
     }
 
-    [string]$secret = (Invoke-Sqlcmd -ServerInstance $Instance -Query "SELECT [value] FROM [_dbaid].[system].[configuration] WHERE [key] = N'COLLECTOR_SECRET'")[0]
-    [string]$7zip = Join-Path $OutputPath "7za.exe"
-    [string]$7zipArgs = "a -mx=9 -tzip -sdel -p'$secret'"
-    [string]$7zipSource = Join-Path $OutputPath "$InstanceTag*.xml" 
-    [string]$7zipTarget = $instanceTag + '_' + (Get-Date -Format 'yyyyMMddHHmmss') + '.zip'
+    if ($ZipXml) {
+        [string]$Secret = Invoke-Sqlcmd -ServerInstance $CollectServer -Database $CollectDatabase -Query "SELECT [value] FROM [_dbaid].[system].[configuration] WHERE [key] = N'COLLECTOR_SECRET'" | Select-Object -ExpandProperty value
+        [string]$7zip = "7za.exe"
+        [string]$7zipArgs = "a -mx=9 -tzip -sdel -p'$Secret'"
+        [string]$7zipSource = Join-Path $OutputXmlFilePath "$InstanceTag*.xml" 
+        [string]$7zipTarget = $InstanceTag + '_' + $Timestamp + '.zip'
 
-    $7zipCmd = "'$7zip' $7zipArgs '$7zipTarget' '$7zipSource'"
+        $7zipCmd = "'$7zip' $7zipArgs '$7zipTarget' '$7zipSource'"
 
-    if ((Get-ChildItem -Path $7zipSource).Length -gt 0) {
-        Invoke-Expression "&$7zipCmd"
-    } else {
-        Write-Host 'No xml files in current directory. '
+        if ((Get-ChildItem -Path $7zipSource).Length -gt 0) {
+            Write-Verbose -Message "Zipping XML file into: $7zipTarget"
+            Invoke-Expression "&$7zipCmd"
+        } else {
+            Write-Error "No xml files found in output directory: $OutputXmlFilePath"
+        }
     }
 }
